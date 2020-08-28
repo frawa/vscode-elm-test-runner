163a24460430b6a4b7037ca5c8d96424d52bbcc9
Frank Wagner <agilecoderfrank@gmail.com>
2020-08-28 21:01:52 +0200

    test walk

--- src/test/util.test.ts
+++ src/test/util.test.ts
@@ -1,8 +1,52 @@
 //import { expect } from 'chai';
 
+import { TestSuiteInfo } from "vscode-test-adapter-api"
+import { walk } from "../util"
+import { expect } from "chai";
+
 describe('util', () => {
 
-    describe.skip('walk suite', () => {
+    describe('walk suite', () => {
+
+        it("no children", () => {
+            const suite: TestSuiteInfo = {
+                type: 'suite',
+                'id': 'a',
+                "label": 'a',
+                children: []
+            }
+            const walked = Array.from(walk(suite))
+            expect(walked).to.eql([suite])
+        });
+
+        it("depth first", () => {
+            const suite: TestSuiteInfo = {
+                type: 'suite',
+                'id': 'a',
+                "label": 'a',
+                children: [{
+                    type: 'suite',
+                    'id': 'a/b',
+                    "label": 'b',
+                    children: [{
+                        type: 'test',
+                        'id': 'a/b/c',
+                        "label": 'c',
+                    }, {
+                        type: 'test',
+                        'id': 'a/b/d',
+                        "label": 'd',
+                    }]
+                }, {
+                    type: 'suite',
+                    'id': 'a/e',
+                    "label": 'e',
+                    children: []
+                }]
+            }
+            const walked = Array.from(walk(suite))
+            expect(walked.map(n => n.label)).to.eql(['a', 'b', 'c', 'd', 'e'])
+        });
     })
 
     describe.skip('parse for lines, by files', () => {
