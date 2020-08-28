a1c15936e05465654c246b419c7804f552681307
Frank Wagner <agilecoderfrank@gmail.com>
2020-08-28 23:18:35 +0200

    add mutation testing

---
+++ stryker.conf.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  mutator: "typescript",
+  packageManager: "yarn",
+  reporters: ["html", "clear-text", "progress"],
+  testRunner: "mocha",
+  transpilers: [],
+  testFramework: "mocha",
+  coverageAnalysis: "perTest",
+  tsconfigFile: "tsconfig.json",
+  mutate: [
+    "src/*.ts",
+    "!src/extension.ts",
+    "!src/adapter.ts",
+    "!src/test/*.ts"
+  ],
+};
